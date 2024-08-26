da7a95a83c3bbfdcdf2ff5ab764fd2fd7c8730d7
tinph010803 <tinphan924@gmail.com>
2024-08-26 09:45:44 +0700

    Tuan01 JS Bai03 Array

---
+++ Tuan01_JS/Bai03_Arrays.js
@@ -0,0 +1,12 @@
+const calcAverageHumanAge = ages =>
+    ages
+      .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+      .filter(age => age >= 18)
+      .reduce((acc, age, _, arr) => acc + age / arr.length, 0);
+  
+  const data1 = [5, 2, 4, 1, 15, 8, 3];
+  const data2 = [16, 6, 10, 5, 6, 1, 4];
+  
+
+  console.log(calcAverageHumanAge(data1)); 
+  console.log(calcAverageHumanAge(data2)); 
